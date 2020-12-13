373444f11a64f86ed063be5ec4868c8b07101508
iyaja <ajayuppili@gmail.com>
2020-12-13 14:47:16 +0530

    feat: output an image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// #[macro_use]
+extern crate approx;
+extern crate nalgebra as na;
+extern crate image;
+
+// use na::{Vector3, Rotation3, Matrix4};
+use image::{ImageBuffer, RgbImage, Rgb};
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut img: RgbImage = ImageBuffer::new(512, 512);
+
+    for (x, y, pix) in img.enumerate_pixels_mut() {
+        let r = (x * 255 / 512) as u8;
+        let g = (y * 255 / 512) as u8;
+        *pix = Rgb([r, g, 64]);
+    }
+    
+    img.save("test.png").unwrap();
+
 }
